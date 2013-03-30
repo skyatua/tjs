ad03bd83f42b8fcce3b7ec96f948403c64eddc20
skyatua <sklyarec_kostya@ukr.net>
2013-03-30 07:19:41 +0000

    On branch master 	modified:   requestHandlers.js 	modified:   router.js 	modified:   server.js

--- server.js
+++ server.js
@@ -5,14 +5,24 @@ function start( route, handle )
 {
 	function onRequest(request, response)
 	{
+	var postData = "";
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request for " + pathname + " recieved.");
 	
-	route(handle, pathname );
-
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello, Tera");
-	response.end();
+	request.setEncoding("utf8");
+	
+	request.addListener("data", 
+		function(postDataChunk)
+		{
+			postData += postDataChunk;
+			console.log("Received POST data chunk '"+
+					postDataChunk + "'.");
+		});
+	request.addListener("end", 
+		function()
+		{
+		route( handle, pathname, response, postData );
+		});
 	}
 	
 	http.createServer(onRequest).listen(8888);
