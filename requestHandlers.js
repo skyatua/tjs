6ccb07fb73bf7ad2486a86edaf84be19f82c1734
skyatua <sklyarec_kostya@ukr.net>
2013-03-30 08:47:36 +0000

    On branch master 	modified:   index.js 	modified:   requestHandlers.js 	renamed:    test.png -> tmp/test.png

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,6 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring"),
+	fs = require("fs");
 
 function start(response, postData)
 {
@@ -36,9 +38,32 @@ function upload(response, postData)
 	console.log("Request handler 'upload' was called.");
 	
 	response.writeHead(200,{"Content-Type": "text/plain"});
-	response.write("You sent:" + postData);
+	response.write("You sent:" + 
+			querystring.parse(postData).text);
 	response.end();
+}
+
+function show(response, postData)
+{
+	console.log("Request handler 'show' was called.");
+	fs.readFile("test.png","binary",
+		function(error, file)
+		{
+			if (error)
+			{
+			response.writeHead(500,{"Content-Type": "text/plain"});
+			response.write(error + "\n");
+			response.end();
+			}
+			else
+			{
+			response.writeHead(500,{"Content-Type": "image/png"});
+                        response.write(file,"binary");
+                        response.end();
+			}
+		});
 }
 
 exports.start  = start;
 exports.upload = upload; 
+exports.show = show;
