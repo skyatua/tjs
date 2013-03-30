e153ebd62c371238aeb99734499132525c8672b5
skyatua <sklyarec_kostya@ukr.net>
2013-03-30 17:52:14 +0000

    On branch test 	modified:   requestHandlers.js 	modified:   router.js 	modified:   server.js

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
-function route( handle, pathname, response, postData )
+function route( handle, pathname, response, request )
 {
 	console.log("About to route request for " + pathname);
 	if (typeof handle[pathname] === 'function')
 	{
-		handle[pathname](response, postData);
+		handle[pathname](response, request);
 	}
 	else
 	{
